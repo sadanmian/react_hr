a9512e024353c9ce41c83e5c5dae50cee689aabf
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-26 20:05:15 +0530

    Fetching News category wise

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,16 +3,51 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News />
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path={"/"} element={<News />} />
+            <Route
+              exact
+              path={"/business"}
+              element={<News key={"technology"} category={"business"} />}
+            />
+            <Route
+              exact
+              path={"/entertainment"}
+              element={
+                <News key={"entertainment"} category={"entertainment"} />
+              }
+            />
+            <Route
+              exact
+              path={"/health"}
+              element={<News key={"health"} category={"health"} />}
+            />
+            <Route
+              exact
+              path={"/science"}
+              element={<News key={"science"} category={"science"} />}
+            />
+            <Route
+              exact
+              path={"/sports"}
+              element={<News key={"sports"} category={"sports"} />}
+            />
+            <Route
+              exact
+              path={"/technology"}
+              element={<News key={"technology"} category={"technology"} />}
+            />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
 }
-
-// 45796de0ad9542109dfd151293f2da9d
