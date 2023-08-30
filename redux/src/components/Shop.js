a1bb0447981db5b8312f6481c12d43f20507251e
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-30 17:51:30 +0530

    Creating a Reducer & Action

--- redux/src/components/Shop.js
+++ redux/src/components/Shop.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Shop() {
   return (
     <div>
-      <h2>Buy this shoes for $50</h2>
+      <h2>Deposit/Withdraw Money</h2>
       <button className="btn btn-primary mx-2">-</button>
-      Add to cart
+      Update Balance
       <button className="btn btn-primary mx-2">+</button>
     </div>
   );
