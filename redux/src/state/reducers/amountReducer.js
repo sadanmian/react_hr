7475c165a6b947f56b19f7afc5281506e94aca5a
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-30 18:11:14 +0530

    Creating a Redux Store

--- redux/src/state/reducers/amountReducer.js
+++ redux/src/state/reducers/amountReducer.js
@@ -1,4 +1,4 @@
-export default reducer = (state = 0, action) => {
+const reducer = (state = 0, action) => {
   if (action.type === "deposit") {
     return state + action.payload;
   }
@@ -8,3 +8,5 @@ export default reducer = (state = 0, action) => {
     return state;
   }
 };
+
+export default reducer;
